3257d472f59a3d9bae5f43c51ef610420e533d79
Boni123git <149221039+Boni123git@users.noreply.github.com>
2023-11-12 11:08:44 +0530

    Update ContainsDuplicate_217_HashMap.java

--- ContainsDuplicate_217_HashMap.java
+++ ContainsDuplicate_217_HashMap.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Map<Integer,Integer> hm = new HashMap<>();
-
+        // Use HashSet instead
         for(int i = 0 ; i < nums.length ; i++){
             if(hm.containsKey(nums[i])){
                 return true;
